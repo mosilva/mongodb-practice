1e036c9f17cb4d853cc5baef9e421446e8c52f17
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-10-11 14:28:15 -0300

    Add project files.

---
+++ TrailMongoDb/AcessMongoDb.cs
@@ -0,0 +1,66 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace TrailMongoDb
+{
+    public class AcessMongoDb
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainAsync(args);
+            Console.WriteLine("Pressione ENTER");
+            Console.ReadKey();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+
+            var doc = new BsonDocument
+            {
+                {"title", "Game of Thrones" }
+            };
+            doc.Add("writer", "George R R Martin");
+            doc.Add("yearRelease", 1999);
+            doc.Add("numberOfPages", 856);
+
+            var genre = new BsonArray();
+            genre.Add("Fantasia");
+            genre.Add("Ação");
+
+            doc.Add("genre", genre);
+
+            Console.WriteLine(doc);
+
+            //Acess to Server
+
+            string connectionString = "mongodb://localhost:27017";
+            IMongoClient client = new MongoClient(connectionString);
+
+            //Acess to Database
+
+            IMongoDatabase dataBaseMongo = client.GetDatabase("MongoData");
+
+            //Acess to Collection
+
+            IMongoCollection<BsonDocument> collection = dataBaseMongo.GetCollection<BsonDocument>("Books");
+
+            //Input document
+
+            await collection.InsertOneAsync(doc);
+
+            Console.WriteLine("Documento incluido");
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ TrailMongoDb/Program.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson;
+
+namespace TrailMongoDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainAsync(args);
+            Console.WriteLine("Pressione ENTER");
+            Console.ReadKey();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            //Console.WriteLine("I'm waiting 10 s...");
+            //await Task.Delay(10000);
+            //Console.WriteLine("I waited 10 s");
+
+
+            var doc = new BsonDocument
+            {
+                {"title", "Game of Thrones" }
+            };
+            doc.Add("writer", "George R R Martin");
+            doc.Add("yearRelease",1999);
+            doc.Add("numberOfPages", 856);
+
+            var genre = new BsonArray();
+            genre.Add("Fantasia");
+            genre.Add("Ação");
+
+            doc.Add("genre", genre);
+
+            Console.WriteLine(doc);
+
+
+        }
+
+
+    }
+}
+
+
