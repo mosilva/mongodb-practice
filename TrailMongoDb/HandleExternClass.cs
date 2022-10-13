9009549e1652dc3a7947ce6acb9569bf20eb8fc4
Marcelo.Oliveira <marcelo.olisi@gmail.com>
2022-10-13 13:27:08 -0300

    Insert books in lots

--- TrailMongoDb/HandleExternClass.cs
+++ TrailMongoDb/HandleExternClass.cs
@@ -11,27 +11,41 @@
 
         static async Task MainClassAsyncConnClass(string[] args)
         {
-            Book book = new Book();
-            book.Title = "The Richest Man In Babylon";
-            book.Writer = "George Samuel Clason";
-            book.YearRelease = 1926;
-            book.NumberOfPages = 160;
 
-            List<string> genres = new List<string>
-            {
-                "Finance"
-            };
+            #region includeWithOnePerTime
+            //Book book = new Book();
+            //book.Title = "The Richest Man In Babylon";
+            //book.Writer = "George Samuel Clason";
+            //book.YearRelease = 1926;
+            //book.NumberOfPages = 160;
 
-            book.Genre = genres;
+            //List<string> genres = new List<string>
+            //{
+            //    "Finance"
+            //};
 
+            //book.Genre = genres;
+            #endregion
+
+            #region includeWithAllProprieties
+
+            //Book book = new Book();
+            //book = BookValues.inputBookValues("Dom Casmurro", "Machado de Assis", 1923, 188, "Romance, Brasilian Liteture")
+
+            #endregion
+
+            List<Book> books = new List<Book>();
+
+            books.Add(BookValues.inputBookValues("The Iliad", "Homer", 1991, 835, "Poetry, Epic"));
+            books.Add(BookValues.inputBookValues(" Art of War", "Sun Tzu",1990, 160, "Treated, Non - fiction"));
 
             //Acess to Connection Class
 
             var conn = new ConnectionMongoDb();
-            
-            await conn.Books.InsertOneAsync(book); 
+;            
+            await conn.Books.InsertManyAsync(books); 
 
-            Console.WriteLine("Documento incluido - finance");
+            Console.WriteLine("Docs include");
 
         }
 
