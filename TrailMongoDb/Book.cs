94dfff7eb0f0b1937f4c33710f53e4b8dfd0f712
Marcelo.Oliveira <marcelo.olisi@gmail.com>
2022-10-13 13:04:53 -0300

    Change book class

--- TrailMongoDb/Book.cs
+++ TrailMongoDb/Book.cs
@@ -14,4 +14,31 @@ namespace TrailMongoDb
         public List<string> Genre { get; set; }
 
     }
+
+    public class BookValues
+    {
+
+        public static Book inputBookValues(string title, string writer, int yearRelease, int numberOfPages, string genre)
+        {
+            Book book = new Book();
+            book.Title = title;
+            book.Writer = writer;
+            book.YearRelease = yearRelease;
+            book.NumberOfPages = numberOfPages;
+            string[] vetGenres = genre.Split(',');
+            List<string> vetGenres2 = new List<string>();
+            for(int i = 0; i < vetGenres.Length; i++)
+            {
+                vetGenres2.Add(vetGenres[i].Trim());
+            }
+            book.Genre = vetGenres2;
+            return book;
+
+        }
+
+
+
+    }
+
+
 }
