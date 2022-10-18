8aff852fc6108e34ff39c7f62f34db827b42106a
Marcelo.Oliveira <marcelo.olisi@gmail.com>
2022-10-18 14:29:03 -0300

    Copy a Blog Project and create conn with MongoDB

---
+++ BlogProject/Models/AcessoMongoDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace projetoBlog.Models
+{
+    public class AcessoMongoDB
+    {
+        public const string CONNECTION_STRING = "mongodb://localhost:27017";
+        public const string DATABASE_NAME = "MongoDataBlog";
+        public const string COLLECTION_NAME = "";
+
+        private static readonly IMongoClient _client;
+
+        //static ConnectionMongoDb()
+        //{
+        //    _client = new Mo
+        //}
+
+    }
+}
---
+++ ProjetoBlog_V2_Por_OK/projetoBlog/Models/AcessoMongoDB.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+
+namespace projetoBlog.Models
+{
+    public class AcessoMongoDB
+    {
+        public const string CONNECTION_STRING = "mongodb://localhost:27017";
+        public const string DATABASE_NAME = "MongoDataBlog";
+        public const string COLLECTION_NAME_PUBLICATION = "Publications";
+        public const string COLLECTION_NAME_USERS = "Users";
+
+
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _database;
+        static AcessoMongoDB()
+        {
+            _client = new MongoClient(CONNECTION_STRING);
+            _database = _client.GetDatabase(DATABASE_NAME);
+        }
+
+        public IMongoClient Client
+        {
+            get { return _client; }
+        }
+
+        public IMongoCollection<Usuario> Users
+        {
+            get
+            {
+                return _database.GetCollection<Usuario>(COLLECTION_NAME_USERS);
+            }
+        }
+
+        public IMongoCollection<Publicacao> Publications
+        {
+            get
+            {
+                return _database.GetCollection<Publicacao>(COLLECTION_NAME_PUBLICATION);
+            }
+        }
+
+
+
+    }
+}
