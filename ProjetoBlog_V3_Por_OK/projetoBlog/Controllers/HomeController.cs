3b6f96a14e40fcfddc60100a3c65460df4d7fe6a
Marcelo <marcelo.olisi@gmail.com>
2022-10-20 15:14:48 -0300

    Create, Read, Update in Mongo DB

---
+++ ProjetoBlog_V3_Por_OK/projetoBlog/Controllers/HomeController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using projetoBlog.Models;
+using projetoBlog.Models.Home;
+using System.Linq.Expressions;
+using MongoDB.Bson;
+
+namespace projetoBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+
+            var conn = new AcessoMongoDB();
+
+            var filter = new BsonDocument();
+            var publicaçõesList = await conn.Publicacoes.Find(filter).SortByDescending(x => x.DataCriacao).Limit(10).ToListAsync();
+
+            var model = new IndexModel
+            {
+                PublicacoesRecentes = publicaçõesList
+            };
+
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult NovaPublicacao()
+        {
+            return View(new NovaPublicacaoModel());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> NovaPublicacao(NovaPublicacaoModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var post = new Publicacao
+            {
+                Autor = User.Identity.Name,
+                Titulo = model.Titulo,
+                Conteudo = model.Conteudo,
+                Tags = model.Tags.Split(' ', ',', ';'),
+                DataCriacao = DateTime.UtcNow,
+                Comentarios = new List<Comentario>()
+
+            };
+
+            var conn = new AcessoMongoDB();
+            await conn.Publicacoes.InsertOneAsync(post);
+
+
+            return RedirectToAction("Publicacao", new {id = post.Id});
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Publicacao(string id)
+        {
+
+            var connectarMongoDB = new AcessoMongoDB();
+            var publicacao = await connectarMongoDB.Publicacoes.Find(x => x.Id == id).SingleOrDefaultAsync();
+
+            if (publicacao == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var model = new PublicacaoModel
+            {
+                Publicacao = publicacao,
+                NovoComentario = new NovoComentarioModel
+                {
+                    PublicacaoId = id
+                }
+            };
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Publicacoes(string tag = null)
+        {
+
+            var conn = new AcessoMongoDB();
+            var pots = new List<Publicacao>();
+
+            if (tag == null)
+            {
+                var filter = new BsonDocument();
+                pots = await conn.Publicacoes.Find(filter).SortByDescending(x => x.DataCriacao).Limit(10).ToListAsync();
+
+            }
+            else
+            {
+                var constructor = Builders<Publicacao>.Filter;
+                var conditonal = constructor.AnyEq(x => x.Tags, tag);
+                pots = await conn.Publicacoes.Find(conditonal).SortByDescending(x => x.DataCriacao).Limit(10).ToListAsync();
+            }
+
+            return View(pots);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> NovoComentario(NovoComentarioModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Publicacao", new { id = model.PublicacaoId });
+            }
+
+            var comment = new Comentario()
+            {
+                Autor = User.Identity.Name,
+                Conteudo = model.Conteudo,
+                DataCriacao = DateTime.UtcNow
+            };
+
+            var conn = new AcessoMongoDB();
+
+
+            var constructor = Builders<Publicacao>.Filter;
+            var conditional = constructor.Eq(x => x.Id, model.PublicacaoId);
+
+            var updateConstructor = Builders<Publicacao>.Update;
+            var updateConditional = updateConstructor.Push(x => x.Comentarios, comment);
+
+            await conn.Publicacoes.UpdateOneAsync(conditional,updateConditional);
+
+            return RedirectToAction("Publicacao", new { id = model.PublicacaoId });
+        }
+    }
+}
---
+++ ProjetoBlog_V3_Por_OK/projetoBlog/Models/AcessoMongoDB.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+
+namespace projetoBlog.Models
+{
+    public class AcessoMongoDB
+    {
+        public const string STRING_DE_CONEXAO = "mongodb://localhost:27017";
+        public const string NOME_DA_BASE = "MongoDataBlog";
+        public const string NOME_DA_COLECAO_PUBLICACAO = "Publicacoes";
+        public const string NOME_DA_COLECAO_USUARIO = "Users";
+
+        private static readonly IMongoClient _cliente;
+        private static readonly IMongoDatabase _BaseDeDados;
+
+        static AcessoMongoDB()
+        {
+            _cliente = new MongoClient(STRING_DE_CONEXAO);
+            _BaseDeDados = _cliente.GetDatabase(NOME_DA_BASE);
+        }
+
+        public IMongoClient Cliente
+        {
+            get { return _cliente; }
+        }
+
+        public IMongoCollection<Publicacao> Publicacoes
+        {
+            get { return _BaseDeDados.GetCollection<Publicacao>(NOME_DA_COLECAO_PUBLICACAO); }
+        }
+
+        public IMongoCollection<Usuario> Usuarios
+        {
+            get { return _BaseDeDados.GetCollection<Usuario>(NOME_DA_COLECAO_USUARIO); }
+        }
+    }
+}
